535968be2af958963148951f685d4d0a57815cf4
omnimonster <121686571+omnimonster@users.noreply.github.com>
2023-11-20 14:16:48 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Tech Stack: 
+
+- Next.js
+- React
+- TypeScript
+- Tailwind CSS
+- Firebase
+- OpenAI API
+- NextAuth
---
+++ firebase.ts
@@ -0,0 +1,18 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db }
